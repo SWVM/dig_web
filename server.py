a6b7928fb04dd37feb14377d242ccd0e76f7fd48
linhan <lli@lli.io>
2021-10-22 20:59:13 -0500

    server...

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import re
+from uuid import uuid4
 from flask import Flask, redirect, url_for, request, render_template
 from flask.helpers import make_response, send_from_directory
-import subprocess
 
 app = Flask(__name__)
 
@@ -14,9 +14,8 @@ def dig_main():
       f = open("/dig_web-master/temp/trace.csv", "w+")
       f.write(request.data.decode("utf-8"))
       f.close()
-      # time ~/miniconda3/bin/python3 -O dig.py  ../tests/traces/cohendiv.csv -log 4
-      subprocess.run(["time", "~/miniconda3/bin/python3", "-0", "/dig_web-master/temp/trace.csv", "-log", "4"])
-      return request.data
+
+      return uuid4()
 
 
 if __name__ == '__main__':
