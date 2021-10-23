f8cca6795db89e4b68e25cd5398a24b79d71d92e
linhan <lli@lli.io>
2021-10-22 20:19:47 -0500

    server again, with subprocess

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import re
 from flask import Flask, redirect, url_for, request, render_template
 from flask.helpers import make_response, send_from_directory
+import subprocess
 
 app = Flask(__name__)
 
@@ -13,7 +14,8 @@ def dig_main():
       f = open("/dig_web-master/temp/trace.csv", "w+")
       f.write(request.data.decode("utf-8"))
       f.close()
-      
+      # time ~/miniconda3/bin/python3 -O dig.py  ../tests/traces/cohendiv.csv -log 4
+      subprocess.run(["time", "~/miniconda3/bin/python3", "-0", "/dig_web-master/temp/trace.csv", "-log", "4"])
       return request.data
 
 
